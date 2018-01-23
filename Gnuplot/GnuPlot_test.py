4b26d1b245e5abc1fef41aea87b2387aa416b499
Adam <Adam.Alenius@gmail.com>
2018-01-23 15:07:37 +0100

    Added my files again

--- Gnuplot/GnuPlot_test.py
+++ Gnuplot/GnuPlot_test.py
@@ -5,7 +5,7 @@ def test():
 
 	g = Gnuplot.Gnuplot(debug = 1)
 	g.title = "This is a test plot"
-	g('set data style linespoints')
+	#g('set data style linespoints')
 	g.plot([[0,1.1], [1,5.8], [2,3.3], [3,4.2]])
 	raw_input("Press any button to continue")
 
