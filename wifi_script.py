d94eff5a87ee742fb07d4244af48f6423cff1319
Adam <Adam.Alenius@gmail.com>
2018-03-07 13:09:37 +0100

    wifi script

---
+++ wifi_script.py
@@ -0,0 +1,14 @@
+from subprocess import call
+import time
+
+call(["nmcli", "device", "disconnect", "wlp2s0"])
+time.sleep(1)
+call(["nmcli", "d" ,"wifi", "list"])
+ssid = raw_input("What wifi do you want to connect to? \n")
+passw = raw_input("What is the network key? (if no key, write na) \n")
+
+if passw == "na":
+    call(["nmcli", "d", "wifi", "connect", ssid])
+else:
+    call(["nmcli", "d", "wifi", "connect", ssid, "password", passw]) 
+    
